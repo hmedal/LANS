cc0d60e1dbc68c6d58b0a7ce0d86842533217d1a
Sarah Harun <me.sarah39@gmail.com>
2017-05-26 09:29:05 -0700

    change txt to json

--- Read_Params.py
+++ Read_Params.py
@@ -1,9 +1,8 @@
 import json
 
-
 class Read_Params:
     Params = {}
 
     def __init__(self):
-        with open('params.txt') as data_file:
+        with open('params.json') as data_file:
             self.Params = json.load(data_file)
