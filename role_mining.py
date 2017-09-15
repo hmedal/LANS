2b56d70bac7172813991504deeb175368c0069c5
Sarah Harun <15017408+sarahharun@users.noreply.github.com>
2017-09-15 12:29:26 -0500

    Updated Malicious node result for Chris

--- role_mining.py
+++ role_mining.py
@@ -8,7 +8,6 @@ import warnings
 import Read_Params as rp
 warnings.filterwarnings("ignore")
 
-
 #Function: Find all input files in given folder
 def find_all_filenames(path_to_dir):
     filenames = listdir(path_to_dir)
@@ -53,21 +52,32 @@ for f in allFiles:
     feature_matrix = []
 
     for i in range(len(botIP_list)):
-        bot_index = feature_data[feature_data['IPs'] == botIP_list[i]].index
-        bot_feature = data[bot_index, 0:6].reshape((1,n_features))
+        bot_data = feature_data[feature_data['IPs'] == botIP_list[i]]
+        bot_feature = np.array(bot_data).reshape((n_features+1,))
+        bot_feature = bot_feature[1:]
         feature_matrix.append(bot_feature)
-        avg_vals = np.mean(feature_matrix, axis=0)
+
+    avg_vals = np.mean(feature_matrix, axis=0)
+    maxs = np.max(feature_matrix, axis=0)
+    mins = np.min(feature_matrix, axis=0)
 
     #Find closest malicious cluster
-    bot_cluster = predictor.predict(avg_vals)
-
+    bot_information=[]
+    bot_information.append(predictor.predict(avg_vals)[0])
+    bot_information.append(len(botIP_list))
+    bot_information.append(mins[0])#Min indegree
+    bot_information.append(maxs[0])#Max indegree
+    bot_information.append(mins[1])#Min outdegree
+    bot_information.append(maxs[1])#Max outdegree
 
     #----------------------------- Write all data -------------------------------#
+
+
     out_file = temp_folder+"role_information"+ f.split('.')[0] +".csv"
     malicious_role_file = temp_folder+"malicious_role"+f.split('.')[0]+".txt"
 
     malicious_role_file = open(malicious_role_file,'w')
-    np.savetxt(malicious_role_file, bot_cluster[np.newaxis], fmt="%d", delimiter=",")
+    np.savetxt(malicious_role_file, bot_information, fmt="%d", delimiter=",")
 
     merge_data = pd.DataFrame({'Node': feature_data['IPs'], 'Role' : y_pred})
     merge_data.to_csv(out_file, sep=',', columns=['Node','Role'],index=False)
